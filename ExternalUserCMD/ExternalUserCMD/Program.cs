4eaafaa2043b5e9b95bfce9efd3273491f82959f
Furkan "Lufzys <53129220+Lufzys@users.noreply.github.com>
2022-02-06 20:56:47 +0300

    Added : Source :100:

---
+++ ExternalUserCMD/ExternalUserCMD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalUserCMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Valve Games - Execute Command";
+        start:
+
+            Console.Clear();
+            Console.Write("\n");
+            Console.Write("  Write Command : ");
+            UserCMD.Execute(Console.ReadLine());
+
+            goto start;
+
+        }
+    }
+}
